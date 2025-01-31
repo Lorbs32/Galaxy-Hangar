8266b6ce554a61ccd767d0b25a68801168081299
Lorbs32 <lorbs32@gmail.com>
2025-01-31 16:24:10 -0500

    initialized with spring

---
+++ hangar-tracker/src/main/kotlin/com/example/spacehangar/hangar_tracker/HangarTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spacehangar.hangar_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HangarTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HangarTrackerApplication>(*args)
+}
---
+++ hangar-tracker/src/test/kotlin/com/example/spacehangar/hangar_tracker/HangarTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spacehangar.hangar_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HangarTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
